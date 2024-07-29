285a8c76fe2dad2981f0950dc07686fe1e80e745
SriG555 <166191193+SriG555@users.noreply.github.com>
2024-07-29 16:14:13 +1000

    Update main.c
    connect doorbell audio code

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include "lpc24xx.h"
 
 extern int sensor(void);
+extern int check_button(void);
 
 int main(void) {
 	while(1){
 		int sensor_value = sensor();
+		//plays a doorbell sound if button p0.10 is pressed 
+		check_button();
 	}
 }
 
