1eefb0d23a15510d3a0ed0753365fde8b530bd3b
SriG555 <166191193+SriG555@users.noreply.github.com>
2024-07-29 16:34:21 +1000

    Update db_check.s
    fixing some minor bugs with the checks in the code

--- db_check.s
+++ db_check.s
@@ -8,15 +8,17 @@
 FIO0PIN     EQU  0x3FFFC014
 
 check_button
-		    mov r1, #1
-			mov r1, r1, lsl #10
-			ldr r2, =FIO0PIN
+		    	mov r1, #1
+			mov r1, r1, lsl #10; create a map for the 10th bit
+			ldr r2, =FIO0PIN; get the address for the pin being accessed 
 press_loop
 			ldr r0, [r2]
-			ands r3, r0, r1
+			ands r0, r1, r3
 			beq pressed 
-			bl play_doorbell 
-pressed
+			; if the dooebell is pressed play doorbell sound
+   			bl play_doorbell 
+      			; insert the LCD function here 
+un_pressed
 			
 			bx pc; return to main
 			END
