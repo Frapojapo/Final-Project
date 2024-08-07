12085497c99f0fe2cadebbf2035f1081ded60eb3
SriG555 <166191193+SriG555@users.noreply.github.com>
2024-08-08 00:37:43 +1000

    added camera-> LCD face

--- db_check.s
+++ db_check.s
@@ -5,6 +5,8 @@
 			IMPORT play_tone
 			IMPORT setup_DAC
 			IMPORT udelay
+   			IMPORT doorbell_display
+      			IMPORT main_page
 FIO0PIN     EQU  0x3FFFC014
 
 check_button
@@ -15,7 +17,11 @@ press_loop
 			ldr r0, [r2]
 			ands r0, r1
 			beq un_pressed 
-			bl play_doorbell
+			bl play_doorbell; play doorbell sound
+   			bl doorbell_display; how face on LCD
+      			ldr r0, =0x2DC6C0;  30^6, for a 3 second delay
+	 		bl udelay; show the face for a short period of time
+    			bl main_page;return image back to main page
 un_pressed
 			
 			bx pc; return to main
